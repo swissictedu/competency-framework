86076d27aeff831ac763ee8b353d0966431564df
Robin <1105080+openscript@users.noreply.github.com>
2019-02-23 11:25:09 +0100

    Enhance validation script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+counter=0
 for file in `find "$PWD"/content/ -name *.xml`; do
     filename=$(basename $file)
     echo -e "+++ Start validation of $filename."
@@ -7,8 +9,15 @@ for file in `find "$PWD"/content/ -name *.xml`; do
 
     if test $? -ne 0; then
         echo "--- Validation of $filename failed."
-        exit 1
+        let counter++
     else
         echo "+++ Validation of $filename succeeded."
     fi
 done
+
+if test $counter -ne 0; then
+    echo "--- $counter files not valid."
+    exit 1
+else
+    echo "+++ All files successfully validated."
+fi
